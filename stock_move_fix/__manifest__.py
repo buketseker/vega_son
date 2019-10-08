f096ccba07122cd81f0011823c5ed045fd9d7d63
Buketseker <buket_skr@hotmail.com>
2019-10-08 16:45:02 +0300

    stock move fix yüklendi.

---
+++ stock_move_fix/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Stock Move Fix",
+    'summary': """Fix for mto + mts""",
+    'description': """
+        Fix for mto + mts
+    """,
+    'author': "Mechsoft",
+    'website': "http://www.mechsoft.com.tr",
+    'category': 'Inventory',
+    'version': '0.1',
+    'depends': ['base', 'stock_mts_mto_rule'],
+    'data': [
+        # 'security/ir.model.access.csv',
+
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+
+    ],
+    'installable': True,
+}
